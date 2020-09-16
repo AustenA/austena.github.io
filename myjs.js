671ceec6a74ca8f5e6acbf40feafc02cddb0515b
Austen <austen.king@trojans.dsu.edu>
2020-09-15 23:33:09 -0500

    Commenting

--- myjs.js
+++ myjs.js
@@ -2,6 +2,8 @@ function linkTo(where){
 	window.top.location.href = "https://austen.dev/" + where;
 }
 
+/*
+Testing
 let batteryPromise = navigator.getBattery();
 batteryPromise.then(batteryCallback);
 
@@ -11,7 +13,9 @@ function batteryCallback(batteryObject) {
 function printBatteryStatus(batteryObject) {
     //console.log("IsCharging", batteryObject.charging);
     console.log("Percentage", batteryObject.level * 100);
-   
+
     //console.log("charging Time", batteryObject.chargingTime);
     //console.log("DisCharging Time", batteryObject.dischargingTime);
 }
+It worked
+*/
